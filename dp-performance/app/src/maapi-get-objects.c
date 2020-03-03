a0467007fbd4366cbcf021f37547e89976b9feee
Conny Ohult Andersen <cohult@cisco.com>
2020-03-03 13:32:17 +0100

    Add nested list support to maapi-get-objects.c
    Signed-off-by: Conny Ohult Andersen <cohult@cisco.com>

--- dp-performance/app/src/maapi-get-objects.c
+++ dp-performance/app/src/maapi-get-objects.c
@@ -15,36 +15,175 @@
 #include <confd_lib.h>
 #include <confd_maapi.h>
 
-/* For debug purposes */
-static void print_value_array(confd_value_t vs[], int n)
+static int entries_per_request = 100;
+static int tv_alloc_len = 0;
+static confd_tag_value_t *tvs = NULL;
+#define TV_BUF_LEN 1024
+
+/* Tag value print helper function */
+static void print_tag_value_array(confd_tag_value_t *val, int nvals,
+                                struct confd_cs_node *start_node,
+                                int start_indent)
 {
+    int i, indent = start_indent;
+    struct confd_cs_node root, *pnode = start_node, *node;
     char tmpbuf[BUFSIZ];
+    char *tmp;
+
+    for (i=0; i<nvals; i++) {
+        if (indent == start_indent && start_node == NULL) {
+            node = confd_find_cs_root(CONFD_GET_TAG_NS(&val[i]));
+            root.children = node;
+            pnode = &root;
+        }
+        switch (CONFD_GET_TAG_VALUE(&val[i])->type) {
+        case C_XMLBEGIN:
+            tmp = "begin";
+            if (pnode != NULL)
+                pnode = confd_find_cs_node_child(pnode, val[i].tag);
+            break;
+        case C_XMLBEGINDEL:
+            tmp = "begin-deleted";
+            if (pnode != NULL)
+                pnode = confd_find_cs_node_child(pnode, val[i].tag);
+            break;
+        case C_XMLEND:
+            tmp = "end";
+            if (pnode != NULL)
+                pnode = pnode->parent;
+            indent -= 2;
+            break;
+        case C_XMLTAG:
+            tmp = "created";
+            break;
+        case C_NOEXISTS:
+            tmp = "deleted";
+            break;
+        default:
+            if (pnode == NULL ||
+                (node = confd_find_cs_node_child(pnode, val[i].tag)) == NULL ||
+                confd_val2str(node->info.type, CONFD_GET_TAG_VALUE(&val[i]),
+                              tmpbuf, sizeof(tmpbuf)) == CONFD_ERR) {
+                confd_pp_value(tmpbuf, sizeof(tmpbuf),
+                               CONFD_GET_TAG_VALUE(&val[i]));
+            }
+            tmp = tmpbuf;
+        }
+        printf("%*s%s %s\n", indent, "",
+               confd_hash2str(CONFD_GET_TAG_TAG(&val[i])), tmp);
+        switch (CONFD_GET_TAG_VALUE(&val[i])->type) {
+        case C_XMLBEGIN:
+        case C_XMLBEGINDEL:
+            indent += 2;
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+static void free_tag_values(confd_tag_value_t *tv, int n)
+{
     int i;
 
-    for (i=0; i<n; i++) {
-      confd_pp_value(tmpbuf, BUFSIZ, &vs[i]);
-      printf("%s ", tmpbuf);
+    for (i = 0; i < n; i++) {
+        confd_free_value(CONFD_GET_TAG_VALUE(&tv[i]));
     }
 }
 
 /* Begin demo code */
+static int traverse_cs_nodes(int ms, int th, int j, char *path, struct confd_cs_node *curr_cs_node, confd_value_t *values, int keyleafs)
+{
+  int nkeys = 0, tot_nobjs, nobjs, curr_nobjs, i, nsubvals, ret, len, sublen, tmpj;
+  u_int32_t *keyptr;
+  int mask = (CS_NODE_IS_ACTION|CS_NODE_IS_PARAM|CS_NODE_IS_RESULT|
+              CS_NODE_IS_NOTIF);
+  confd_value_t *subvals;
+  struct maapi_cursor mc;
+  struct confd_cs_node *cs_node;
+
+  if (j > (tv_alloc_len / 2)) {
+    tv_alloc_len *= 2;
+    tvs = (confd_tag_value_t *) realloc(tvs, sizeof(confd_tag_value_t) * tv_alloc_len);
+  }
+
+  len = strlen(path);
+  int nval = 0;
+  for (cs_node = curr_cs_node; cs_node != NULL; cs_node = cs_node->next, nval++) {
+    snprintf(&path[len], BUFSIZ - len, "/%s:%s", confd_ns2prefix(cs_node->ns), confd_hash2str(cs_node->tag));
+    if (cs_node->info.flags & CS_NODE_IS_LIST) {
+      nsubvals = confd_max_object_size(cs_node);
+      tot_nobjs = maapi_num_instances(ms, th, path);
+      subvals = malloc(sizeof(confd_value_t) * nsubvals * tot_nobjs);
+      curr_nobjs = 0;
+      maapi_init_cursor(ms, th, &mc, path);
+      nobjs = entries_per_request;
+      do {
+        ret = maapi_get_objects(&mc, &subvals[curr_nobjs * nsubvals], nsubvals, &nobjs);
+        curr_nobjs += nobjs;
+      } while (ret >= 0 && mc.n != 0);
+      nkeys = 0;
+      for (keyptr = cs_node->info.keys; *keyptr != 0; keyptr++) {
+        nkeys++;
+      }
+      sublen = strlen(path);
+      for (i = 0; i < tot_nobjs; i++) {
+        confd_format_keypath(&path[sublen], BUFSIZ - sublen, "{%*x}", nkeys, &subvals[i * nsubvals]);
+        CONFD_SET_TAG_XMLBEGIN(&tvs[j], cs_node->tag, cs_node->ns); j++;
+        tmpj = j;
+        j = traverse_cs_nodes(ms, th, j, path, cs_node->children, &subvals[i * nsubvals], nkeys);
+        if (j == tmpj) {
+          j--;
+        } else {
+          CONFD_SET_TAG_XMLEND(&tvs[j], cs_node->tag, cs_node->ns); j++;
+        }
+      }
+      free(subvals);
+    } else if (cs_node->info.flags & CS_NODE_IS_CONTAINER) {
+      nsubvals = confd_max_object_size(cs_node);
+      subvals = malloc(sizeof(confd_value_t) * nsubvals);
+      maapi_get_object(ms, th, subvals, nsubvals, path);
+
+      CONFD_SET_TAG_XMLBEGIN(&tvs[j], cs_node->tag, cs_node->ns); j++;
+      tmpj = j;
+      j = traverse_cs_nodes(ms, th, j, path, cs_node->children, subvals, 0);
+      if (j == tmpj) {
+        j--;
+      } else {
+        CONFD_SET_TAG_XMLEND(&tvs[j], cs_node->tag, cs_node->ns); j++;
+      }
+      free(subvals);
+    } else if ((cs_node->info.flags & mask) == 0) {
+      if (values != NULL) {
+        if ((cs_node->info.flags & CS_NODE_IS_WRITE) == 0 || keyleafs > 0) {
+          CONFD_SET_TAG_VALUE(&tvs[j], cs_node->tag, &values[nval]); j++;
+          if (keyleafs > 0)
+            keyleafs--;
+        }
+      } else {
+        confd_fatal("A root node must be a container or list node, not a leaf node\n");
+      }
+    }
+    path[len] = 0;
+  }
+  return j;
+}
+
 int main(int argc, char *argv[])
 {
   struct sockaddr_in addr;
-  struct maapi_cursor mc;
-  int c, i, thandle, maapisock, values_per_entry, ret;
-  int nobj;
-  confd_value_t *v, inkeys[1];
-  struct confd_cs_node *object;
+  int c, thandle, maapisock, j, ret;
+  struct confd_cs_node *cs_node;
   struct confd_ip ip;
   const char *user = "admin", *groups[] = { "admin" }, *context = "system";
-  char *inkey = NULL;
-  char *path = "/r:sys/routes/inet/route";
+  char path[BUFSIZ], *lastslash;
   char *confd_ip = "127.0.0.1";
   int confd_port = CONFD_PORT;
-  int debuglevel = CONFD_DEBUG, entries_per_request = 100;
+  int debuglevel = CONFD_DEBUG;
 
-  while ((c = getopt(argc, argv, "a:p:u:g:c:P:k:e:drts")) != EOF) {
+  strcpy(&path[0], "/r:sys");
+
+  while ((c = getopt(argc, argv, "a:p:u:g:c:P:e:drts")) != EOF) {
     switch(c) {
     case 'a':
       confd_ip = optarg;
@@ -62,10 +201,7 @@ int main(int argc, char *argv[])
       context = optarg;
       break;
     case 'p':
-      path = optarg;
-      break;
-    case 'k':
-      inkey = optarg;
+      strcpy(&path[0], (char *)optarg);
       break;
     case 'e':
       entries_per_request = atoi(optarg);
@@ -95,9 +231,6 @@ int main(int argc, char *argv[])
                          sizeof (struct sockaddr_in)) != CONFD_OK)
     confd_fatal("Failed to load schemas from ConfD\n");
 
-  object = confd_cs_node_cd(NULL, path);
-  values_per_entry = confd_max_object_size(object);
-
   if ((maapisock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     confd_fatal("Failed to create the MAAPI socket");
   if (maapi_connect(maapisock, (struct sockaddr*)&addr,
@@ -119,33 +252,21 @@ int main(int argc, char *argv[])
     confd_fatal("Failed to start trans\n");
   }
 
-  if(maapi_init_cursor(maapisock, thandle, &mc, path) != CONFD_OK)
-    confd_fatal("maapi_init_cursor() failed\n");
-
-  if(inkey != NULL) {
-    CONFD_SET_STR(&inkeys[0], &inkey[0]);
-    if (maapi_find_next(&mc, CONFD_FIND_SAME_OR_NEXT, inkeys, 1) != CONFD_OK)
-      confd_fatal("maapi_find_next() failed\n");
-    if(mc.n == 0) {
-      fprintf(stderr, "Key \"%s\" not found\n", inkey);
-      exit(0);
-    }
+  if ((ret = maapi_set_flags(maapisock, thandle, MAAPI_FLAG_NO_DEFAULTS)) != CONFD_OK) {
+    confd_fatal("maapi_set_flags() failed\n");
   }
 
-  v = malloc(sizeof(confd_value_t) * values_per_entry * entries_per_request);
-  do {
-    nobj = entries_per_request;
-    ret = maapi_get_objects(&mc, v, values_per_entry, &nobj);
-    if (ret >= 0) {
-      for (i = 0; i < nobj; i++) {
-        printf("%d: ", i);
-        print_value_array(&v[i*values_per_entry], values_per_entry);
-        printf("\n");
-      }
-    } else {
-      confd_fatal("maapi_get_objects() failed\n");
-    }
-  } while (ret >= 0 && mc.n != 0);
-  free(v);
-  maapi_destroy_cursor(&mc);
+  cs_node = confd_cs_node_cd(NULL, &path[0]);
+  tv_alloc_len = TV_BUF_LEN;
+  tvs = (confd_tag_value_t *) malloc(sizeof(confd_tag_value_t) * tv_alloc_len);
+  j = 0;
+  if((lastslash = strrchr(&path[0], '/')) != NULL) {
+    *lastslash = 0;
+  }
+
+  j = traverse_cs_nodes(maapisock, thandle, j, &path[0], cs_node, NULL, 0);
+
+  print_tag_value_array(tvs, j, cs_node, 0);
+  //free_tag_values(tvs,j);
+  //free(tvs);
 }
