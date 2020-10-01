c690383dce5376529a6ab1341b24436c7ee8b9a9
Conny Ohult Andersen <cohult@cisco.com>
2020-10-01 17:58:18 +0200

    Added tagging to enable resolving conflicting node trees manually
    Signed-off-by: Conny Ohult Andersen <cohult@cisco.com>

--- ann2ann-stmt/app/yin_ann_stmt.py
+++ ann2ann-stmt/app/yin_ann_stmt.py
@@ -3,15 +3,16 @@ import os
 from bs4 import BeautifulSoup
 import fileinput
 import argparse
+import copy
 
 def pattern_replace(file_path, pattern, subst):
     for line in fileinput.input(file_path, inplace=True):
         print(line.replace(pattern, subst), end='')
 
 
-def handle_annotate_node(annotate_node, module_soup, ann_soup, append_node, module_prefix, prev_tag_name):
+def handle_annotate_node(annotate_node, module_soup, ann_soup, append_node, module_prefix):
     path_tags = annotate_node['target'].split('/') # Split the "target" path into a "path_list" of prefix:node strings.
-    if path_tags[0] == '':
+    if path_tags[0] == '': # Remove the extra '' if there was a root slash
         path_tags.pop(0)
     path_tag_name = path_tags[-1].split(':')[1] # Find and create a list of all module_nodes in module named as the last tag.
     path_tags.pop()
@@ -19,16 +20,14 @@ def handle_annotate_node(annotate_node, module_soup, ann_soup, append_node, modu
     for module_node in module_soup.find_all(yname=path_tag_name):
         module_nodes.append(module_node)
     ann_nodes = [] # Create a new "tailf_annotate_statement" tag list entry for each matching node in the module_nodes list
-    for module_node in module_nodes:
-        annotate_statement = ann_soup.new_tag("tailf:annotate-statement", statement_path="{}[name=\'{}\']".format(module_node.name, module_node['yname']))
+    for module_node in list(module_nodes):
+        annotate_statement = ann_soup.new_tag('tailf:annotate-statement', statement_path="{}[name=\'{}\']".format(module_node.name, module_node['yname']))
         for child in annotate_node.find_all(recursive=False): # Add the content to be annotated to the annotate_statement
-            if child.name == "annotate": # Nested tailf:annotate
-                prev_tag_name = handle_annotate_node(child, module_soup, ann_soup, annotate_statement, module_prefix, prev_tag_name)
-                if module_node.parent.name == "choice": # Handle the "extra" node after a choice statement in tailf:annotate
-                    for choice_ann_stmt in annotate_statement.find_all(recursive=False):
-                        choice_ann_stmt['extra_choice_node'] = 'remove'
+            if child.name == 'annotate' or child.name == 'tailf:annotate': # Nested tailf:annotate
+                annotate_statement = handle_annotate_node(child, module_soup, ann_soup, annotate_statement, module_prefix)
             else:
-                annotate_statement.append(child)
+                child_clone = copy.copy(child) # Clone as we need to decompose the tailf:annotate below
+                annotate_statement.append(child_clone)
         ann_nodes.append(annotate_statement)
     annotate_node.decompose() # Done with current tailf:annotate traversal. Remove the tailf:annotate node from the annotation module
     while path_tags: # Get the next "prefix:node" string from the "path_tags" list
@@ -38,30 +37,48 @@ def handle_annotate_node(annotate_node, module_soup, ann_soup, append_node, modu
         if path_tag_prefix == module_prefix:
             path_tag_name = path_tag[1]
             for module_node, ann_node in zip(list(module_nodes), list(ann_nodes)): # Compare module node tag name with path tag name to find the intended path
+                ann_index = ann_nodes.index(ann_node)
                 if module_node.name == 'augment' or module_node.name == 'grouping': # Handle groupings and augment
                     path_tags.clear() # Done
+                    for node in ann_nodes: # Eliminate other non-intended paths
+                        if node is not ann_node:
+                            ann_nodes.remove(node)
                     break;
-                if module_node.parent.name != "choice" or (path_tag_name != prev_tag_name and prev_tag_name != ''): # Handle choice
-                    if module_node.parent.name == 'grouping' or module_node.parent.name == 'augment' or module_node.parent['yname'] == path_tag_name:
-                        module_nodes[module_nodes.index(module_node)] = module_node.parent
-                        attribute_name = 'yname'
-                        if module_node.parent.name == 'augment': # Handle that augment nodes have a different attribute name than other nodes
-                            attribute_name = 'target_node'
-                        annotate_statement = ann_soup.new_tag("tailf:annotate-statement", statement_path="{}[name=\'{}\']".format(module_node.parent.name, module_node.parent[attribute_name]))
-                        index = ann_nodes.index(ann_node)
-                        ann_soup.module.insert_after(ann_nodes[index])
-                        annotate_statement.append(ann_nodes[index])
-                        ann_nodes[index] = annotate_statement
+                elif ((module_node.parent.name == "choice" and module_node.parent['yname'] == path_tag_name) or (module_node.name == "choice" and module_node['yname'] == path_tag_name) or (module_node.parent.name == "choice" and module_node.parent['yname'] == path_tags[-1].split(':')[1])) and module_node.name != 'case': # Handle choice without a case
+                    module_nodes[module_nodes.index(module_node)] = module_node.parent
+                    new_annotate_statement = ann_soup.new_tag("tailf:annotate-statement", statement_path="{}[name=\'{}\']".format(module_node.parent.name, module_node.parent['yname']))
+                    if module_node.parent.name == "choice" and module_node.parent['yname'] == path_tag_name:
+                        extra_choice_node = ann_soup.new_tag('remove_next_node', extra_choice_node=module_node['yname'])
+                        extra_choice_node.append(ann_nodes[ann_index])
+                        new_annotate_statement.append(extra_choice_node)
                     else:
-                        module_nodes.remove(module_node) # Eliminated
-                        ann_nodes.remove(ann_node)
+                        new_annotate_statement.append(ann_nodes[ann_index])
+                    ann_nodes[ann_index] = new_annotate_statement
+                elif module_node.parent.name == 'grouping' or module_node.parent.name == 'augment' or (module_node.parent['yname'] == path_tag_name):
+                    module_nodes[module_nodes.index(module_node)] = module_node.parent
+                    attribute_name = 'yname'
+                    if module_node.parent.name == 'augment': # Handle that augment nodes have a different attribute name than other nodes
+                        attribute_name = 'target_node'
+                    new_annotate_statement = ann_soup.new_tag("tailf:annotate-statement", statement_path="{}[name=\'{}\']".format(module_node.parent.name, module_node.parent[attribute_name]))
+                    new_annotate_statement.append(ann_nodes[ann_index])
+                    ann_nodes[ann_index] = new_annotate_statement
+                else:
+                    module_nodes.remove(module_node) # Eliminated
+                    ann_nodes.remove(ann_node)
         else:
             if path_tags: # No longer in the module to be annotated. Exit while loop by clearing the list
                 path_tags.clear()
-        prev_tag_name = path_tag_name
-    if(len(ann_nodes) > 0):
+    len_ann_nodes = len(ann_nodes)
+    if len_ann_nodes > 1:
+        i = 0
+        for node in ann_nodes:
+            confict_container = ann_soup.new_tag("conflict", number="{} of {}".format(i+1, len_ann_nodes))
+            confict_container.append(node)
+            append_node.append(confict_container)
+            i += 1
+    elif len_ann_nodes > 0:
         append_node.append(ann_nodes[0]) # Append the tailf:annotate-statement to the tailf:annotate-module node
-    return path_tag_name;
+    return append_node;
 
 
 def trim_yin(module_file_name, ann_file_name):
@@ -82,8 +99,10 @@ def trim_yin(module_file_name, ann_file_name):
     annotate_module = ann_soup.new_tag("tailf:annotate-module", module_name="{}".format(module_name_str)) # Create the top tailf:annotate-module tag in the annotation module
     ann_soup.module.append(annotate_module) # Append the tailf:annotate-module to the "module" root tag
     for annotate_node in ann_soup.module.find_all('tailf:annotate', recursive=False): # Find all tags named "tailf:annotate" in the ann_soup, i.e. soup of annotations.
-        handle_annotate_node(annotate_node, module_soup, ann_soup, annotate_module, module_prefix, "")
-    for elem in ann_soup.find_all(extra_choice_node='remove'): # Remove all "extra" choice statement nodes
+        handle_annotate_node(annotate_node, module_soup, ann_soup, annotate_module, module_prefix)
+    for elem in ann_soup.find_all('remove_next_node'): # Remove all "extra" choice statement nodes
+        for child in elem.find_all(recursive=False):
+            child.unwrap()
         elem.unwrap()
     with open("{}mod".format(ann_file_name), "w") as fp:
         fp.write(str(ann_soup))
@@ -106,5 +125,4 @@ if __name__ == "__main__":
     parser.add_argument('filename', type=str,
                         help='<file> YIN module to be annotated')
     args = parser.parse_args()
-    #print("Input: {} {}".format(args.filename, args.tailf_annotation_module))
     trim_yin(args.filename, args.tailf_annotation_module)
