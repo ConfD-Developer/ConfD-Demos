4f61ec3f7286a2dd190f32c37f93363bfadd95c9
Conny Ohult Andersen <cohult@users.noreply.github.com>
2021-08-25 22:10:08 +0200

    Update run.sh

--- ann-stmt/app/run.sh
+++ ann-stmt/app/run.sh
@@ -2,14 +2,17 @@
 rm -rf yang
 mkdir -p yang
 
-echo "Create a tailf:annotate-module/statement module and move over the tailf extensions from the original YANG module"
+echo "*** Create a tailf:annotate-module/statement module and move over the tailf extensions from the original YANG module"
 for f in yang-orig/*.yang
 do
-    echo "Processing $f"
+    echo "*** Processing $f"
     python3 tailf_ann_stmt.py -t -m -w -i -x -a -u -p -o yang $f
 done
 
 make stop clean all start
 
-echo "tail -F devel.log"
+echo "*** resulting annotation modules"
+cat yang/*-ann.yang
+
+echo "*** tail -F devel.log"
 tail -F devel.log
